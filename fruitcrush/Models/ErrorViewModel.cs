baa2063523912bc4ae91c65fa705395cb86b02fd
kamil kaan <kamil kaan@DESKTOP-C0KBEAB>
2026-02-16 03:03:31 +0300

    Proje dosyası ekle.

---
+++ fruitcrush/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fruitcrush.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ fruitcrush/Models/GameModel.cs
@@ -0,0 +1,11 @@
+﻿namespace fruitcrush.Models
+{
+    public class GameModel
+    {
+        public int Level { get; set; }
+        public int Score { get; set; }
+        public int Moves { get; set; }
+        public int TimeLeft { get; set; }
+    }
+}
+
